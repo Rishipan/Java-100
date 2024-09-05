2561045c7a2aa683e386287450543d5e0edaf611
Rishikesh <rishikeshpanditrp@gmail.com>
2024-09-05 05:36:23 +0530

    04

---
+++ 01. Getting Started/Get04.java
@@ -0,0 +1,20 @@
+// Sum of first n natural numbers
+
+import java.util.Scanner;
+
+public class Get04 {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter Staring number: ");
+            int n1= sc.nextInt();
+
+
+            System.out.print("Enter ending number: ");
+            int n2= sc.nextInt();
+
+            int sum=((n2*(n2+1))/2)-((n1*(n1-1))/2);
+
+            System.out.println("Sum : "+ sum);
+        }
+    }
+} 
